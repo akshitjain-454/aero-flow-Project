72ab28db4a53c156533bf531d62d5f1d06aee4a9
Danny-0-0 <grdn1483@leeds.ac.uk>
2026-02-26 23:11:11 +0000

    UserTable edit

--- ktor/src/main/kotlin/com/flightbooking/models/TableModels.kt
+++ ktor/src/main/kotlin/com/flightbooking/models/TableModels.kt
@@ -1,8 +1,7 @@
 package com.flightbooking.models
 
 data class UsersTable(
-    val userId: Int,
-    val roomId: Int,
+    val id: Int,
     val firstname: String,
     val lastname: String,
     val email: String,
